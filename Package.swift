010cf175800176cde9831f09c280cb77c585fa3d
Sevki <s@sevki.org>
2022-05-14 14:55:45 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,4 +2,16 @@ import PackageDescription
 
 let package = Package(
   name: "Pitchy"
+  products: [
+      .library(name: "Pitchy", targets: ["Pitchy"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+      .target(
+          name: "Pitchy",
+      .testTarget(
+          name: "PitchyTests",
+          dependencies: ["Pitchy"]),
+  ]
 )
